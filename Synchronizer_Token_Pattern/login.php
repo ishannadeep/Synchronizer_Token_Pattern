0754b063fde1a5b548f7bdad9bee570c6e727f86
ishan <ishan@DESKTOP-IDDG36R>
2018-09-10 01:26:55 +0530

    setting the index.html & login.php

--- Synchronizer_Token_Pattern/login.php
+++ Synchronizer_Token_Pattern/login.php
@@ -1,8 +1,26 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Login Authentication</h1>
+        <?php
+        if($_POST['userName']=="ishan123" && $_POST['password']=="ishan123")
+        {
+            session_start();
+            $sessionID= session_id();
+            echo '<form action="server.php" method="POST">';
+            echo '<div><label>name</label><br><input type="text" name="name"> </div>';
+            
+        }
+        ?>
+    </body>
+</html>
 
