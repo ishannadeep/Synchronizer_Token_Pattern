ac2afb9e6e417b3052231c33bc317e63fd5c8d54
ishan <ishan@DESKTOP-IDDG36R>
2018-09-10 20:06:36 +0530

    setting the cookie & CSRF Token

--- Synchronizer_Token_Pattern/login.php
+++ Synchronizer_Token_Pattern/login.php
@@ -12,13 +12,50 @@ and open the template in the editor.
     <body>
         <h1>Login Authentication</h1>
         <?php
-        if($_POST['userName']=="ishan" && $_POST['password']=="ishan123")
-        {
+        if ($_POST['userName'] == "ishan" && $_POST['password'] == "ishan123") {
+            $Name = $_POST['userName'];
+            echo 'session start';
             session_start();
-            $sessionID= session_id();
-            echo '<form action="server.php" method="POST">';
-            echo '<div><label>name</label><br><input type="text" name="name"> </div>';
-            
+            $sessionID = session_id();
+            $cookie_name = "admin";
+            $cookie_value = $Name;
+            $cookie = setcookie($cookie_name, $cookie_value, time() + (86400 ), "/"); // 86400 = 1 day
+
+            if (isset($_COOKIE[$cookie_name])) {
+                echo 'cookie is set';
+                echo $_COOKIE[$cookie_name];
+
+                function token($name, $cookie_name) {
+                    if (!session_id()) {
+                        session_start();
+                    }
+                    $sessionID = session_id();
+                    return sha1($name . $sessionID);
+                }
+
+                echo '<form action="server.php" method="POST">';
+                echo '<div><label>name</label><br><input type="text" name="name"> </div>';
+                echo '<div><label>phone number</label><br><input type="text" name="phonenum"> </div>';
+                echo '<div><label>address</label><br><input type="text" name="address"> </div>';
+                echo '<div><br><input type="hidden" value=';
+                echo token($Name, $cookie);
+                echo ' name="csrf_token"> </div>';
+                echo '<div><br><input type="hidden" value=';
+                echo $Name;
+                echo ' name="form_name"> </div>';
+                echo '<div><br><input type="hidden" value=';
+                echo $cookie_name;
+                echo ' name="cookie_name"> </div>';
+                echo '<div><br><input type="submit" > </div>';
+                echo '<div><br><input type="hidden" value=';
+                echo $sessionID;
+                echo ' name="sessionID"> </div>';
+                echo '</form>';
+            } else {
+                echo 'cookie is not set';
+            }
+        } else {
+            echo '<script>alert("User name & Password Incorrect");</script>';
         }
         ?>
     </body>
--- Synchronizer_Token_Pattern/server.php
+++ Synchronizer_Token_Pattern/server.php
@@ -10,9 +10,34 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <h1>haii</h1>
+        <h1>Server</h1>
         <?php
-        // put your code here
+        $cookie_Name = $_POST['cookie_name'];
+
+//         include 'login.php';
+        function token($name, $cookie_name) {
+
+            return sha1($name . $_POST['sessionID']);
+        }
+
+        function checktoken($token, $name, $cookie_namE) {
+            return $token === token($name, $cookie_namE);
+        }
+
+        if (isset($_COOKIE[$cookie_Name])) {
+            if (!empty($_POST['csrf_token'])) {
+                if (checktoken($_POST['csrf_token'], $_POST['form_name'], $cookie_Name)) {
+                    echo '<h1>Form is Succesfully Authenticated</h1>';
+                    echo $_COOKIE[$cookie_Name];
+                } else {
+                    echo '<h1>Form is  Authentication is faild</h1>';
+                }
+            } else {
+                echo '<h1>token is empty</h1>';
+            }
+        } else {
+            echo 'cookie is not set';
+        }
         ?>
     </body>
 </html>
