8faeb49ca39486eed78a9b4096e8337d89513709
ishan <ishan@DESKTOP-IDDG36R>
2018-09-22 21:17:10 +0530

    solved some errors

--- Synchronizer_Token_Pattern/login.php
+++ Synchronizer_Token_Pattern/login.php
@@ -16,6 +16,25 @@ function checktoken($token, $name, $sessionID) {
     return $token === token($name, $sessionID);
 }
 
+function onc() {
+    if ($_POST['userName'] == "ishan" && $_POST['password'] == "ishan123") {
+        $Name = $_POST['userName'];
+        echo 'session start';
+        session_start();
+        $sessionID = session_id();
+        $cookie_name = "user";
+        $cookie_value = $Name;
+        setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+
+        $Token = token($Name, $sessionID);
+        $_SESSION["token"] = $Token;
+        header("Location: form.html"); /* Redirect browser */
+    } else {
+        echo '<script>alert("User name & Password Incorrect");</script>';
+        header("Location: login.html"); /* Redirect browser */
+    }
+}
+
 if (isset($_COOKIE["user"])) {
     echo 'session start' + $_COOKIE["user"];
     session_start();
@@ -25,45 +44,8 @@ if (isset($_COOKIE["user"])) {
     $_SESSION["token"] = $Token;
     header("Location: form.html"); /* Redirect browser */
 } else {
-
-    function onc() {
-        if ($_POST['userName'] == "ishan" && $_POST['password'] == "ishan123") {
-            $Name = $_POST['userName'];
-            echo 'session start';
-            session_start();
-            $sessionID = session_id();
-            $cookie_name = "user";
-            $cookie_value = $Name;
-            setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
-
-            $Token = token($Name, $sessionID);
-            $_SESSION["token"] = $Token;
-            header("Location: form.html"); /* Redirect browser */
-        } else {
-            echo '<script>alert("User name & Password Incorrect");</script>';
-            header("Location: index.html"); /* Redirect browser */
-        }
-    }
-
+    onc();
 }
 ?>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <div>
-            <form action="http://localhost:8080/Synchronizer_Token_Pattern/Synchronizer_Token_Pattern/login.php" method="post" onsubmit="<?php onc() ?>">
-                <label>user name</label>
-                <input type="text" name="userName" style="width: 100px;"><br>
-                <label>Password</label>
-                <input type="password" name="password" style="width: 100px;"><br>
-                <input type="submit" value="go" >
 
-            </form><br>
-
-        </div>
-    </body>
-</html>
 
